5f9b92097fc0fbf19dacdac0fed2fe3e92d9373f
Oleg Fedorov <akademuk24@gmail.com>
2022-09-23 14:57:54 +0300

    aa

--- script/burger.js
+++ script/burger.js
@@ -2,3 +2,9 @@ $(".birger-button").on("click", function(){
   $(".birger-button").toggleClass("close-btn");
   $(".menu").toggleClass("open-menu");
 });
+$(".birger-button").click(function(){
+  $(".body").addClass("bodyActive");
+  $(".birger-button").click(function(){
+    $(".body").removeClass("bodyActive");
+  });
+});
