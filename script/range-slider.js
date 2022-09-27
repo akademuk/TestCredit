0ce142d09d0fccd9fc78c1887497c63b85b23605
Oleg Fedorov <akademuk24@gmail.com>
2022-09-27 10:51:57 +0300

    ss

--- script/range-slider.js
+++ script/range-slider.js
@@ -6,12 +6,12 @@ let sum = document.getElementById("resultPercent");
 const valuePercent = 0.0001;
 
 function rangeMoney(){
-    valPercent = (valueMoney.value / valueMoney.max)*100;
-    valueMoney.style.background = `linear-gradient(to right, #8BD16A ${valPercent}%, #C6BAFB ${valPercent}%)`;
+    rangePercent = (valueMoney.value / valueMoney.max)*100;
+    valueMoney.style.background = `linear-gradient(to right, #8BD16A ${rangePercent}%, #C6BAFB ${rangePercent}%)`;
     innerMoney.textContent = valueMoney.value;
 
-    valPercent = (valueDate.value / valueDate.max)*100;
-    valueDate.style.background = `linear-gradient(to right, #8BD16A ${valPercent}%, #C6BAFB ${valPercent}%)`;
+    rangeDate = (valueDate.value / valueDate.max)*100;
+    valueDate.style.background = `linear-gradient(to right, #8BD16A ${rangeDate}%, #C6BAFB ${rangeDate}%)`;
     innerDate.textContent = valueDate.value;
 
     letDate = (Number(valueDate.value) * Number(valuePercent));
@@ -23,6 +23,6 @@ function rangeMoney(){
     var days = valueDate.value;
     
     var newDate = new Date(Date.now()+days*24*60*60*1000)
-    sumDate.textContent = newDate.toLocaleDateString;
+    sumDate.textContent = newDate.toLocaleDateString('de-DE');
 }
 rangeMoney();
